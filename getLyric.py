84f2423d53708c9d3d8b7ce75a184f76d60ec556
Misato Watanabe <watamisa080513@gmail.com>
2022-07-11 17:18:40 +0900

    改行ごとで歌詞を取得する関数の追加

--- getLyric.py
+++ getLyric.py
@@ -10,7 +10,7 @@ API_KEY = os.getenv('MUSIXMATCH_API_KEY')
 def get_lyric(title, artist):
     track_request_url = 'http://api.musixmatch.com/ws/1.1/track.search'
     track_request_params = {'apikey': API_KEY,
-                            'q_track': "Habit", 'q_artist': "SEKAI NO OWARI"}
+                            'q_track': title, 'q_artist': artist}
     track_response = requests.get(
         track_request_url, params=track_request_params)
     track_info = track_response.json()
@@ -24,9 +24,16 @@ def get_lyric(title, artist):
     return lyric
 
 
-def get_formated_lyric(title, artist):
+def get_line_splitlyric(title, artist):
     lyric = get_lyric(title, artist)
     lyric_section_div = re.split('\n\n', lyric)
+    # MUST：有料APIにしたら削除する(無料枠ではここまでと文字が出力されているため)
+    lyric_section_div.pop(-1)
+    return lyric_section_div
+
+
+def get_formated_lyric(title, artist):
+    lyric_section_div = get_line_splitlyric(title, artist)
     lyric_sp = [[] for i in range(len(lyric_section_div))]
     for i in range(len(lyric_section_div)):
         lyric_sp[i] = lyric_section_div[i].replace('.', ' ').split()
