af04802b05b326cf39344869035ecbf1730fcf03
Misato Watanabe <watamisa080513@gmail.com>
2022-07-19 01:10:52 +0900

    APIエラーの対応（仮)

--- getLyric.py
+++ getLyric.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 API_KEY = os.getenv('MUSIXMATCH_API_KEY')
 
+"""
+TODO:全体的にエラーハンドリングが必要。どんな不足データが返ってくるか考えること。
+"""
+
 
 def get_lyric(title, artist):
     track_request_url = 'http://api.musixmatch.com/ws/1.1/track.search'
@@ -14,18 +18,29 @@ def get_lyric(title, artist):
     track_response = requests.get(
         track_request_url, params=track_request_params)
     track_info = track_response.json()
+    # TODO:エラーハンドリング
+    if track_info["message"]["header"]["status_code"] != 200:
+        return None
+    if len(track_info["message"]["body"]["track_list"]) == 0:
+        return None
     track_id = track_info["message"]["body"]["track_list"][0]["track"]["track_id"]
 
     lyric_request_url = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get'
     lyric_request_params = {'apikey': API_KEY, 'track_id': track_id}
     lyric_response = requests.get(lyric_request_url, lyric_request_params)
+    # TODO:エラーハンドリング
     lyric_info = lyric_response.json()
+    if lyric_info["message"]["header"]["status_code"] != 200:
+        return None
     lyric = lyric_info["message"]["body"]["lyrics"]["lyrics_body"]
+
     return lyric
 
 
 def get_line_splitlyric(title, artist):
     lyric = get_lyric(title, artist)
+    if lyric is None:
+        return None
     lyric_section_div = re.split('\n\n', lyric)
     # MUST：有料APIにしたら削除する(無料枠ではここまでと文字が出力されているため)
     lyric_section_div.pop(-1)
@@ -34,6 +49,8 @@ def get_line_splitlyric(title, artist):
 
 def get_formated_lyric(title, artist):
     lyric_section_div = get_line_splitlyric(title, artist)
+    if lyric_section_div is None:
+        return None
     lyric_sp = [[] for i in range(len(lyric_section_div))]
     for i in range(len(lyric_section_div)):
         lyric_sp[i] = lyric_section_div[i].replace('.', ' ').split()
