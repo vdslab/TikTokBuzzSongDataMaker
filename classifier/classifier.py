b0a8c59cb2fbf590dc5f7f2a1d922c133ef0a37c
Misato Watanabe <watamisa080513@gmail.com>
2022-07-23 01:49:26 +0900

    データ整形の修正

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -3,19 +3,32 @@ from api import getAllPastSongs
 
 
 # TODO:このデータの整形をなしの形でapiからとってきたい
-def formatData():
+def formatData(data):
     music_feature_key = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
                          'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
     s_data = []
     for d in data:
         obj = dict()
-        for key in music_feature_key:
-            if d["detail"].get("music_feature"):
+        obj["rank"] = d["rank"]
+        if d["detail"].get("music_feature"):
+            for key in music_feature_key:
                 obj[key] = d["detail"]["music_feature"][key]
+        else:
+            continue
+         # TODO:歌詞データがないのを省くでいいのかどうか（現状は省いている）
         if d["detail"].get("lyrics_feature"):
-            obj["total_rhyme_score"] = d["detail"]["lyrics_feature"]["total_rhyme_score"]
-            obj["total_positive_score"] = d["detail"]["lyrics_feature"]["total_positive_score"]
+            if d["detail"]["lyrics_feature"]["total_rhyme_score"] is None:
+                continue
+            else:
+                obj["total_rhyme_score"] = d["detail"]["lyrics_feature"]["total_rhyme_score"]
+            if d["detail"]["lyrics_feature"]["total_positive_score"] is None:
+                continue
+            else:
+                obj["total_positive_score"] = d["detail"]["lyrics_feature"]["total_positive_score"]
+        else:
+            continue
         s_data.append(obj)
+    return s_data
 
 
 data = getAllPastSongs()
