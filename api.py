2c8e08e568bf02ecc73fb1dc6cc5a53dabb386ed
Misato Watanabe <watamisa080513@gmail.com>
2022-07-26 20:41:15 +0900

    分類できたデータをデータベースに追加

---
+++ api.py
@@ -0,0 +1,55 @@
+import json
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def getAllSongsId():
+    url = "https://tsubame.hasura.app/api/rest/songs/id"
+    headers = {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
+    }
+
+    res = requests.get(url, headers=headers)
+    data = res.json()
+    songs_id = data["songs"]
+
+    return songs_id
+
+
+def post_past_songs_one(data):
+    url = "https://tsubame.hasura.app/api/rest/insert/past_songs_one"
+    headers = {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
+    }
+    data = json.dumps({"object": data})
+    res = requests.post(url, headers=headers, data=data)
+    data = res.json()
+    print(data)
+
+
+def post_songs_one(data):
+    url = "https://tsubame.hasura.app/api/rest/insert/songs_one"
+    headers = {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
+    }
+    data = json.dumps({"object": data})
+    res = requests.post(url, headers=headers, data=data)
+    data = res.json()
+    print(data)
+
+
+def post_buzz_songs_one(data):
+    url = "https://tsubame.hasura.app/api/rest/insert/buzz_songs_one"
+    headers = {
+        "Content-Type": "application/json",
+        "x-hasura-admin-secret": os.getenv('HASURA_ADMIN_SECRET'),
+    }
+    data = json.dumps({"object": data})
+    res = requests.post(url, headers=headers, data=data)
+    data = res.json()
+    print(data)
---
+++ insertDb.py
@@ -0,0 +1,56 @@
+import json
+from api import getAllSongsId
+from memo import res
+from api import post_buzz_songs_one, post_songs_one
+import time
+
+
+#songsデータと, past_songsデータに分ける
+def div_data(data):
+    songs = []
+    buzz_songs = []
+
+    ids = getAllSongsId()
+    ids = [d["id"] for d in ids]
+
+    for d in data:
+        hadId = d["id"] in (ids)
+        if hadId:
+            continue
+
+        song = dict()
+        song["id"] = d["id"]
+        song["title"] = d["title"]
+        song["preview_url"] = d["preview_url"]
+        song["artist"] = d["artist"]
+        song["artist_uri"] = d["artist_uri"]
+        song["genres"] = json.dumps(d["genres"])
+        song["music_feature"] = json.dumps(d["music_feature"])
+        song["lyrics_feature"] = json.dumps(d["lyrics_feature"])
+
+        buzz_song = dict()
+        buzz_song["id"] = d["id"]
+        buzz_song["date"] = d["date"]
+        buzz_song["rank"] = d["rank"]
+
+        songs.append(song)
+        buzz_songs.append(buzz_song)
+
+    return {"songs": songs, "buzz_songs": buzz_songs}
+
+
+# TODO:for文じゃなくて一括で行けるようなmutationを作る
+def insertDb(data):
+    divided_data = div_data(data)
+
+    for d in divided_data["buzz_songs"]:
+        post_buzz_songs_one(d)
+        time.sleep(0.5)
+
+    print("------------------")
+
+    for d in divided_data["songs"]:
+        post_songs_one(d)
+        time.sleep(0.5)
+
+    print("post all fin")
