cf60e2488ddc16efa6ace5b3468bd917838cd4bd
Misato Watanabe <watamisa080513@gmail.com>
2022-07-27 00:54:47 +0900

    コメントの追記

--- filterPopularSong.py
+++ filterPopularSong.py
@@ -13,6 +13,7 @@ def filter_popular_song(file_name):
         for row in dic_reader:
             rank = int(row["\ufeffrank"])
             pre_rank = int(row["previous_rank"])
+            # TODO:過去n位以内に入ったことがあるものは除くとかをしてもいいかもしれない
             if pre_rank == -1:
                 filterd_data.append(row)
             elif -1*(rank-pre_rank) >= 15:
