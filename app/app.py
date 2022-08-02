4b72c1225cf36b869baf7cba3efa36678ef82758
Misato Watanabe <watamisa080513@gmail.com>
2022-08-03 00:19:50 +0900

    曲情報取得APIの作成

---
+++ app/app.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from pathlib import Path
+parent_file_path = str(Path('__file__').resolve().parent)
+sys.path.append(parent_file_path)
+from flask import Flask, jsonify
+from flask_cors import CORS
+from buzzSongMaker.addFeatureAndFormatedData import get_song_info
+
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route("/song_info/<song_id>",  methods=['GET'])
+def get_song_feature(song_id):
+    song_featur = get_song_info(song_id)
+    return jsonify(song_featur)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',
+            port=int(os.environ.get('PORT', 8080)))
--- buzzSongMaker/addFeatureAndFormatedData.py
+++ buzzSongMaker/addFeatureAndFormatedData.py
@@ -1,4 +1,8 @@
+import sys
 import os
+file_path = os.path.dirname(__file__);
+sys.path.append(file_path)
+
 from tkinter.tix import ROW
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -71,3 +75,53 @@ def add_feature_and_formated_data(data_list, date):
         time.sleep(1.5)
 
     return formated_data_list
+
+
+def get_song_info(id):
+    formated_data = dict()
+    analysis_song_info = dict()
+
+    song_features = sp.audio_features(id)
+    song_track = sp.track(id)
+    artists_info = song_track["artists"][0]
+    preview_url = song_track["preview_url"]
+    img_url = song_track["album"]["images"][0]["url"]
+
+    formated_data["id"] = id
+    formated_data["title"] = song_track["name"]
+    formated_data["preview_url"] = preview_url
+    formated_data["img_url"] = img_url
+    formated_data["artist"] = artists_info["name"]
+    formated_data["artist_uri"] = artists_info["uri"][15:]
+
+    music_feuture = dict()
+    for key in json_key_name:
+        music_feuture[key] = song_features[0][key]
+    formated_data["music_feature"] = music_feuture
+
+    lyric_text = get_formated_lyric(
+        song_track["name"], artists_info["name"])
+    if lyric_text is None:
+        lyrics_feature = None
+    else:
+        lyrics_feature = analysis_lyric(lyric_text)
+
+    # TODO：歌詞分析
+    formated_data["lyrics_feature"] = lyrics_feature
+
+    artist_name = artists_info["name"]
+    artist_result = sp.search(
+        q='artist:' + artist_name, type='artist')
+    artist_info_list = artist_result["artists"]["items"]
+    analysis_song_info["artist"] = artist_name
+
+    # Noneにしておくことでspotify上で検索ができてないことが後から確認できるように
+    if len(artist_info_list) == 0:
+        analysis_song_info["artist_uri"] = None
+        formated_data["genres"] = None
+
+    if len(artist_info_list) > 0:
+        analysis_song_info["artist_uri"] = artist_info_list[0]["uri"][15:]
+        formated_data["genres"] = artist_info_list[0]["genres"]
+
+    return formated_data
